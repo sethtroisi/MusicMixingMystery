9807731de5a02217ae3134b12ce21c082b62766d
Seth Troisi <sethtroisi@google.com>
2022-10-31 15:53:25 -0700

    Converting "What Pitch" quiz to macro templates
    This will help with Medium / Hard quiz

--- src/main.py
+++ src/main.py
@@ -16,6 +16,10 @@ from routes.hello_world import Welcome
 
 api, app = create_app()
 
+# Have happier debugging
+app.jinja_env.trim_blocks = True
+app.jinja_env.lstrip_blocks = True
+
 # For routing rest requests
 #api.add_resource(Welcome, "/", "/hello")
 
